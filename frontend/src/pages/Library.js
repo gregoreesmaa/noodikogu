b0cb56196b763556e45b586dbe172589e2011034
rannilo <33835169+rannilo@users.noreply.github.com>
2018-09-17 17:28:09 +0300

    Tegin statistika avalehele nähtavaks

--- frontend/src/pages/Library.js
+++ frontend/src/pages/Library.js
@@ -1,20 +1,65 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import List from '@material-ui/core/List';
 import TextField from '@material-ui/core/TextField';
-import { BackendService } from '../services';
-import { PieceItem, PlaylistItem } from '../components';
-import { playlistsLoaded, changeFilterCriteria, filteredListChanged } from '../state/';
+import {BackendService} from '../services';
+import {PieceItem, PlaylistItem} from '../components';
+import {playlistsLoaded, changeFilterCriteria, filteredListChanged, setStatistics} from '../state/';
 
 class Library extends Component {
   constructor(props) {
     super(props);
-
+    BackendService.getStatistics()
+      .then(response => this.props.setStatistics(response.data));
     this.componentDidUpdate({});
   }
 
+  renderStatistics() {
+    let mostPopularBrowser;
+    let browserPopularity = 0;
+    for(let key in this.props.statistics.browserPopularity){
+      let tempPopularity = this.props.statistics.browserPopularity[key];
+      if (tempPopularity > browserPopularity){
+        browserPopularity = tempPopularity;
+        mostPopularBrowser = key;
+      }
+    }
+
+    let mostPopularOpSystem;
+    let opSystemPopularity = 0;
+    for(let key in this.props.statistics.opSystemPopularity){
+      let tempPopularity = this.props.statistics.opSystemPopularity[key];
+      if (tempPopularity > opSystemPopularity){
+        opSystemPopularity = tempPopularity;
+        mostPopularOpSystem = key;
+      }
+    }
+
+    let mostPopularTime;
+    let timePopularity = 0;
+    for(let key in this.props.statistics.timePopularity){
+      let tempPopularity = this.props.statistics.timePopularity[key];
+      if (tempPopularity > timePopularity){
+        timePopularity = tempPopularity;
+        mostPopularTime = key;
+      }
+    }
+
+    return (
+      <div>
+        <h4>Külastajate statistika</h4>
+        {mostPopularBrowser} on selle lehe külastajate seas populaarseim brauser ning seda on kasutatud {browserPopularity} korda.
+        <br></br>
+        {mostPopularOpSystem} on selle lehe külastajate seas populaarseim operatsioonisüsteem ning seda on kasutataud {opSystemPopularity} korda.
+        <br></br>
+        {mostPopularTime}:00-{Number(mostPopularTime)+1}:00 on selle lehe külastajate seas populaarseim külastusaeg ning sellel ajal külastajaid on {timePopularity}.
+      </div>
+    );
+  }
+
   render() {
+
     console.log("filteredList", this.props.filteredList);
     return (<div className='container'>
       <TextField
@@ -30,12 +75,13 @@ class Library extends Component {
         {this.props.filteredList
           ? this.props.filteredList.map((item, index) =>
             item.type === 'partituur'
-              ? (<PieceItem key={index} piece={item} />)
-              : (<PlaylistItem key={index} playlist={item} />))
+              ? (<PieceItem key={index} piece={item}/>)
+              : (<PlaylistItem key={index} playlist={item}/>))
           : this.props.playlists.map((item, index) =>
-            (<PlaylistItem key={index} playlist={item} />))
+            (<PlaylistItem key={index} playlist={item}/>))
         }
       </List>
+      <div>{this.renderStatistics()}</div>
     </div>);
   }
 
@@ -49,7 +95,7 @@ class Library extends Component {
       if (this.props.filter && this.props.filter.length >= 2) {
         BackendService.search(this.props.filter)
           .then(response => {
-            let { partituurid, repertuaarid } = response.data;
+            let {partituurid, repertuaarid} = response.data;
             partituurid.forEach(partituur => partituur.type = 'partituur');
             repertuaarid.forEach(repertuaar => repertuaar.type = 'repertuaar');
 
@@ -66,7 +112,18 @@ class Library extends Component {
   }
 }
 
-const mapStateToProps = ({ user, filter, filteredList, playlists }) => ({ user, filter, filteredList, playlists });
-const mapDispatchToProps = dispatch => bindActionCreators({ playlistsLoaded, changeFilterCriteria, filteredListChanged }, dispatch);
+const mapStateToProps = ({user, filter, filteredList, playlists, statistics}) => ({
+  user,
+  filter,
+  filteredList,
+  playlists,
+  statistics
+});
+const mapDispatchToProps = dispatch => bindActionCreators({
+  playlistsLoaded,
+  changeFilterCriteria,
+  filteredListChanged,
+  setStatistics
+}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Library);
